d65ca373a1ce6e954b257f8673156ca40f4c262e
Madelyn Deshield <madelynwallop@gmail.com>
2019-10-23 03:15:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    binding.pry
+   return true
+  else speed == 40 || 60
+    return false
+end
 end
 
 
 
 def not_safe?(speed)
+   speed < 40 || speed > 60 ? true : false
+   
+  
 	
 end
-	
+
 
 
