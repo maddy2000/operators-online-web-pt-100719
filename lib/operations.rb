3b79710f5aea9995accacf1b1862a912fa379299
Madelyn Deshield <madelynwallop@gmail.com>
2019-10-23 03:16:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+   return true
+  else speed == 40 || 60
+    return false
+end
 end
 
 
 
 def not_safe?(speed)
+   speed < 40 || speed > 60 ? true : false
+   
+  
 	
 end
-	
+
 
 
